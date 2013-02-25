17a485c5804cc39e877afabaf91311151dddd869
Julien <julien.glijer@free.fr>
2013-02-25 16:41:07 +0100

    nouvelle modif des javascript + rectif du contact.html

--- assets/www/js/infosContact.js
+++ assets/www/js/infosContact.js
@@ -5,12 +5,11 @@ document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
 
 	Name = extractUrlParams ("nom");
-	console.log("Le nom qui est retourne est : " + Name);
 	var options = new ContactFindOptions();
 	options.filter= Name;
 	options.multiple = true;
 	var fields = ["displayName", "photos", "phoneNumbers", "id"];
-	navigator.contacts.find(fields, onSuccess, onError, options);
+	navigator.contacts.find(fields, onSuccessContact, onError, options);
 	
 }
 
@@ -25,4 +24,34 @@ function extractUrlParams (name) {
         return "";
     else
         return results[1];
+}
+
+function onSuccessContact(contacts) {
+
+	console.log("Le contact est :" + contacts[0] );
+	if (contacts[0].photos && contacts[0].photos[0].type == "url") {
+		code = '<img src="'+contacts[0].photos[0].value+'" class="photo_contact">';
+					}
+	else {
+		code = '<img src="img/photo.jpg" class="photo_contact">';
+				 	
+	}
+	if (contacts[0].phoneNumbers) 
+	{
+		code = code + contacts[0].displayName + '<br>'
+		  		 	+ '<a href="' + contacts[0].phoneNumbers[0].value + '"  data-role="button" data-mini="true" data-inline="true">Appeler</a>';
+	}
+	else 
+	{
+		code = code + contacts[0].displayName + '<br>';
+	}
+	
+	$("#info_contact").append(code);
+    
+}
+
+// onError: Failed to get the contacts
+
+function onError(contactError) {
+    alert('onError!');
 }
